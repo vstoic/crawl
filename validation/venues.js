08e6cd4b91853eee5a379f2d343af751ff0d78f0
App Academy Student <email@email.com>
2022-08-29 14:17:09 -0400

    add validations to venues and crawls

---
+++ validation/venues.js
@@ -0,0 +1,33 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateVenueInput(data) {
+  let errors = {};
+
+  data.name = validText(data.name) ? data.name : '';
+  if (Validator.isEmpty(data.name)) {
+    errors.text = 'Name is required for Venue';
+  }
+
+  data.cost = validText(data.cost) ? data.cost : '';
+  if (Validator.isEmpty(data.cost)) {
+    errors.cost = 'Cost is required for the Venue';
+  }
+
+  data.address = validText(data.address) ? data.address : '';
+  if (Validator.isEmpty(data.address)) {
+    errors.address = 'Address is required for Venue';
+  }
+  
+  if (Validator.isEmpty(data.longitude)) {
+    errors.longitude = 'Longitude is required for Venue';
+  }
+
+  if (Validator.isEmpty(data.latitude)) {
+    errors.latitude = 'Latitude is required for Venue';
+  }
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  };
+};
