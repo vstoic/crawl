2d99d69f61a29bd716939f6c832b67b1af04334c
Tom Leger <tomleger@gmail.com>
2022-08-31 15:37:20 -0400

    fized crawl api util

--- frontend/src/util/crawl_api_util.js
+++ frontend/src/util/crawl_api_util.js
@@ -1,20 +1,19 @@
 import axios from "axios";
 
 export const fetchAllCrawls = () => {
-    return axios.get("/api/crawls/");
+  return axios.get("/api/crawls/");
 };
 
-export const fetchCrawl = id => {
-    return axios.get(`/api/crawls/${id}`);
+export const fetchCrawl = (id) => {
+  return axios.get(`/api/crawls/${id}`);
 };
-
 
 export const updateCrawl = (crawl) => {
-    return axios.patch(`/api/crawls/${crawl.id}`, crawl);
-    
-export const createCrawl = async (crawl) => {
-    let crawlAll = await axios.post('/api/crawls');
-    console.log("CrawlAll=======>",crawlAll)
-    return crawlAll
+  return axios.patch(`/api/crawls/${crawl.id}`, crawl);
+};
 
+export const createCrawl = async (crawl) => {
+  let crawlAll = await axios.post("/api/crawls");
+  console.log("CrawlAll=======>", crawlAll);
+  return crawlAll;
 };
