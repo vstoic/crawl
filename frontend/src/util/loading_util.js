4d20486e733cbe7c6f6fa75bbd7ee18a61e821f8
Sing Cheng <Sing.cheng@outlook.com>
2022-08-31 14:01:00 -0400

    add website to venue

---
+++ frontend/src/util/loading_util.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoadingContainer = () => {
+    return (
+        <div className='loading-container'>
+            <img src={LoadingGif} alt="" />
+        </div>
+    )
+}
+
+
+export default LoadingContainer
