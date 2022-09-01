aea374a979e7386c0191f7aa348e8a7ea9dd770c
App Academy Student <email@email.com>
2022-09-01 16:54:10 -0400

    basic skeleton for profile

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -1,6 +1,7 @@
 import * as UserUtil from "../util/user_util";
 
 export const RECEIVE_USER = "RECEIVE_USER";
+export const RECEIVE_USERS = "RECEIVE_USERS";
 export const REMOVE_USER = "REMOVE_USER";
 export const UPDATE_CURRENT_USER = "UPDATE_CURRENT_USER";
 export const RECEIVE_USER_ERRORS = "RECEIVE_USER_ERRORS";
@@ -9,6 +10,11 @@ export const CLEAR_USER_ERRORS = "CLEAR_USER_ERRORS";
 const receiveUser = user => ({
     type: RECEIVE_USER,
     user
+});
+
+const receiveUsers = users => ({
+    type: RECEIVE_USERS,
+    users
 });
 
 const updateCurrentUser = user => ({
@@ -32,6 +38,10 @@ export const clearUserErrors = () => ({
 
 export const fetchUser = userId => dispatch => (
     UserUtil.fetchUser(userId).then((user) => dispatch(receiveUser(user)))
+);
+
+export const fetchUsers = () => dispatch => (
+    UserUtil.fetchUsers().then((users) => dispatch(receiveUsers(users)))
 );
 
 export const updateUser = userData => dispatch => (
