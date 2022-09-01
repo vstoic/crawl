19c981bd103754bcb88d476334c3fd9a6aead06f
tara <tara_m_oliver@yahoo.com>
2022-09-01 18:07:27 -0400

    rendering main

--- frontend/src/components/main/main.js
+++ frontend/src/components/main/main.js
@@ -1,17 +1,19 @@
 // src/components/main/main_page.js
 
 import React from "react";
-import NavBarContainer from "../nav/navbar_container"
 import "../../assets/stylesheets/main.css";
+import CrawlShow from "../crawls/crawl_show";
+import MainPageItem from "./main_page_item";
 
 
 class MainPage extends React.Component {
 
 componentDidMount(){
-  
+  this.props.fetchAllCrawls();
 }
 
   render() {
+    // debugger
     return (
       <div className="main-page-container">
         <div className="main-left-container">
@@ -22,16 +24,22 @@ componentDidMount(){
         </div>
         <div className="main-crawl-container">
           <div className="crawl-container">
-              <h1>Crawl Name</h1>
-              <h2>Website</h2>
-              <div className="crawl-venue-container">
-                <li>Venue Name1</li>
-                <li>Venue Name1</li>
-                <li>Venue Name1</li>
-              </div>
-              <div className="mp-image-container">
-                <img src="" className="main-page-image"/>
-              </div>
+             <ul>
+                {
+                  this.props.crawls.map(crawl => (
+                    <MainPageItem
+                    key={crawl.id}
+                    title={crawl.title}
+                    time={crawl.time}
+                    distance={crawl.distance}
+                    cost={crawl.cost}
+                    venues={crawl.venues}
+                    venueReducer={this.props.venueReducer}
+                    fetchAllVenues={this.props.fetchAllVenues}
+                    />
+                  ))
+                }
+             </ul>
           </div>
         </div>
       </div>
