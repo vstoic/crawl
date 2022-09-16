ec5f95ba135a30ecf60a241a4cf4d229b2bd687b
Tom Leger <tomleger@gmail.com>
2022-09-16 14:23:04 -0400

    updated profile

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -5,52 +5,52 @@ import { Link } from "react-router-dom";
 class Profile extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { user: this.props.currentUser , imageSet:null}
+    this.state = { user: this.props.currentUser, imageSet: null };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.props.fetchUser(this.props.match.params.id);
-     this.props.fetchCrawlByUser(this.props.match.params.id)
+    this.props.fetchCrawlByUser(this.props.match.params.id);
     this.props.fetchAllCrawls();
-   
   }
-getImageUrl = async(image) => {
+  getImageUrl = async (image) => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "uqb0krjs");
+    data.append("cloud_name", "dhudcmiwm");
+    fetch("  https://api.cloudinary.com/v1_1/dhudcmiwm/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        let userData = {
+          id: this.props.match.params.id,
+          profileImage: data.url,
+        };
+        this.props.updateImage(userData);
 
-
-const data = new FormData();
-data.append("file", image);
-data.append("upload_preset", "uqb0krjs");
-data.append("cloud_name", "dhudcmiwm");
-fetch("  https://api.cloudinary.com/v1_1/dhudcmiwm/image/upload", {
-  method: "post",
-  body: data,
-})
-  .then((resp) => resp.json())
-  .then((data) => {
-    let userData = {
-      id: this.props.match.params.id,
-      profileImage:data.url
-    };
-    this.props.updateImage(userData)
-   
-    // setUrl(data.url);
-  })
-  .catch((err) => console.log(err));
-}
-  render () {
-      console.log(
-        "ImageData========>",
-        this.props.crawlsReducer?.crawlByUser?.data
-      );
-      this.props.crawlsReducer?.crawlByUser?.data?.sort((a,b) => (b.votecount) -  (a.votecount))
-      let profileImage =
-        this.props.userFetch?.currentUser != undefined
-          ? this.props.userFetch?.currentUser?.data?.profileImage
-          : this.props.viewedUser?.profileImage;
-    if (this.props.viewedUser) return (
-      <div className="profile-container">
-        <div className="profile-left">
-          {/* {this.props.userFetch?.currentUser != undefined && (
+        // setUrl(data.url);
+      })
+      .catch((err) => console.log(err));
+  };
+  render() {
+    console.log(
+      "ImageData========>",
+      this.props.crawlsReducer?.crawlByUser?.data
+    );
+    this.props.crawlsReducer?.crawlByUser?.data?.sort(
+      (a, b) => b.votecount - a.votecount
+    );
+    let profileImage =
+      this.props.userFetch?.currentUser != undefined
+        ? this.props.userFetch?.currentUser?.data?.profileImage
+        : this.props.viewedUser?.profileImage;
+    if (this.props.viewedUser)
+      return (
+        <div className="profile-container">
+          <div className="profile-left">
+            {/* {this.props.userFetch?.currentUser != undefined && (
             <img
               className="profile-img"
               src={this.props.userFetch?.currentUser?.data?.profileImage}
@@ -59,43 +59,53 @@ fetch("  https://api.cloudinary.com/v1_1/dhudcmiwm/image/upload", {
             />
           )} */}
 
-          <img
-            className="profile-img"
-            src={profileImage}
-            alt='<img className="personal-link-photo"
+            <img
+              className="profile-img"
+              src={profileImage}
+              alt='<img className="personal-link-photo"
              />'
-          />
+            />
 
-          <input
-            type="file"
-            accept="images/*"
-            onChange={(e) => this.getImageUrl(e.target.files[0])}
-          />
-          <h1> {this.props.viewedUser.username}'s Page</h1>
-        </div>
+            <input
+              type="file"
+              accept="images/*"
+              onChange={(e) => this.getImageUrl(e.target.files[0])}
+            />
+            <h1> {this.props.viewedUser.username}'s Page</h1>
+          </div>
 
-        <div className="profile-right">
-          {this.props.crawlsReducer?.crawlByUser?.data?.length == 0 && (
-            <div className="profile-right-crawls">No crawls here</div>
-          )}
-          {(this.props.crawlsReducer?.crawlByUser?.data || []).map(
-            (item, index) => (
-              <div key={item._id} className="profile-right-crawls">
-                <Link to={`/crawl/${item._id}`}>{item.title} </Link>
-                {item.category}
-                {item.time}
+          <div className="profile-right">
+            {this.props.crawlsReducer?.crawlByUser?.data?.length == 0 && (
+              <div className="profile-right-crawls">
+                No crawls here
+                <div>
+                  <Link to={"/crawlCreate/"} className="nav-login-button">
+                    Create a Crawl
+                  </Link>
+                </div>
               </div>
-            )
-          )}
-          {/* <div className="profile-right-crawls">First Crawl goes here</div>
+            )}
+            {(this.props.crawlsReducer?.crawlByUser?.data || []).map(
+              (item, index) => (
+                <div key={item._id} className="profile-right-crawls">
+                  <Link to={`/crawl/${item._id}`}>{item.title} </Link>
+                  category: {item.category}
+                  time: {item.time}
+                  description: {item.description}
+                  cost: {item.cost}
+                  votes: {item.votecount}
+                </div>
+              )
+            )}
+            {/* <div className="profile-right-crawls">First Crawl goes here</div>
           <div className="profile-right-crawls">Second Crawl goes here</div>
           <div className="profile-right-crawls">Third Crawl goes here</div>
           <div className="profile-right-crawls">Fourth Crawl goes here</div>
           <div className="profile-right-crawls">Fifth Crawl goes here</div>
           <div className="profile-right-crawls">Sixth Crawl goes here</div> */}
+          </div>
         </div>
-      </div>
-    );
+      );
   }
 }
 
