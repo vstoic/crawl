8d25fab8262d16cf65a3d941edfdfb1c19773f4d
mohammad_rizwan <rizwanm76@yahoo.com>
2022-10-21 15:48:18 -0400

    heroku testing

--- frontend/src/googlekey.js
+++ frontend/src/googlekey.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./googlekey_dev.js');
+    module.exports = require('./googlekey_dev');
 } else {
-    module.exports = require('./googlekey_dev.js');
+    module.exports = require('./googlekey_dev');
 };
 
 // For Windows Machines, the googlekey files above must have ".js" ending. Leave this ending off for Mac computers.
