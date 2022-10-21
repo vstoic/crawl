dfdf56f599f633c2dd1e4faf6fc2b8c6787dd87c
mohammad_rizwan <rizwanm76@yahoo.com>
2022-10-21 15:46:02 -0400

    testing heroku Google API Key for Maps

--- frontend/src/googlekey.js
+++ frontend/src/googlekey.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./googlekey_prod');
+    module.exports = require('./googlekey_dev.js');
 } else {
-    module.exports = require('./googlekey_dev');
+    module.exports = require('./googlekey_dev.js');
 };
 
 // For Windows Machines, the googlekey files above must have ".js" ending. Leave this ending off for Mac computers.
