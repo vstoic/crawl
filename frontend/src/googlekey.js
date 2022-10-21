87339111b464c032ef000a29b987914a9e0e5ebd
mohammad_rizwan <rizwanm76@yahoo.com>
2022-10-21 15:51:17 -0400

    maps development

--- frontend/src/googlekey.js
+++ frontend/src/googlekey.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./googlekey_dev');
+    module.exports = require('./googlekey_prod');
 } else {
-    module.exports = require('./googlekey_dev');
+    module.exports = require('./googlekey_prod');
 };
 
 // For Windows Machines, the googlekey files above must have ".js" ending. Leave this ending off for Mac computers.
